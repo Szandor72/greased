4726a521953911c324499c6b4cccdb4747d986cf
Steve Buikhuizen <steve@Steves-MBP.lan>
2016-12-31 08:56:15 +1100

    CHORE initial commit of Assertion components

---
+++ src/aura/greased_AssertionEquals/greased_AssertionEqualsController.js
@@ -0,0 +1,15 @@
+({
+    doInit: function(component, event, helper) {
+        var deep = DeepDiff.noConflict();
+
+        var diff = deep.diff;
+
+        var expected = component.get("v.expected");
+        var actual = component.get("v.actual");
+        var differences = diff(expected, actual);
+        var space = 3;
+        component.set("v.lhs", JSON.stringify(expected, helper.replacer, space));
+        component.set("v.rhs", JSON.stringify(actual, helper.replacer, space));
+        component.set("v.diff",JSON.stringify(differences, helper.replacer, space));
+    }
+})
