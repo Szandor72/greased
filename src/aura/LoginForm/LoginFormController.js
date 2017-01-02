3795e3a97ad26624d91d274b21f77fba9f9294d9
Steve Buikhuizen <steve@Steves-MacBook-Pro.local>
2017-01-02 17:16:38 +1100

    FEATURE sample LoginForm now has full load/login behaviour ready for testing

--- src/aura/LoginForm/LoginFormController.js
+++ src/aura/LoginForm/LoginFormController.js
@@ -1,6 +1,23 @@
 ({
-    handleStartTests: function(component, event, helper) {
-        console.log("starting login tests");
-        console.log(event.getParams());
+    doInit: function (component, event, helper) {
+        helper.sendToService(component, "c.load", {}, function (loadResult) {
+            console.log("loading username: "+loadResult.username);
+            component.set("v.username", loadResult.username);
+        });
+    },
+    adjust: function (component, event, helper) {
+        var un = component.get("v.username");
+        var pw = component.get("v.password");
+        component.set("v.disabled", $A.util.isEmpty(un) || $A.util.isEmpty(pw));
+    },
+    doLogin: function (component, event, helper) {
+        component.set("v.message", "Logging you in..");
+        helper.sendToService(component, "c.login", {
+            username: component.get("v.username"),
+            password: component.get("v.password")
+        }, function (loginResult) {
+            $A.log(loginResult);
+            component.set("v.message", loginResult.MSG);
+        });
     }
 })
